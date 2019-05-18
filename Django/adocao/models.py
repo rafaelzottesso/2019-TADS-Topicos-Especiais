3c9f8b396b78b90f76c28ee4188e7b5817928768
Aluno <rafael.zottesso@ifpr.edu.br>
2019-05-17 21:08:01 -0300

    Criação de modelos e registro deles no /admin.

--- Django/adocao/models.py
+++ Django/adocao/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Estado(models.Model):
+    # nome_do_atributo = models.Tipo(configuração)
+    sigla   = models.CharField(max_length=2)
+    nome    = models.CharField(max_length=50)
+
+    # Como se fosse toString e self = this
+    def __str__(self):
+        return self.sigla + ' - ' + self.nome
+
+
+class Cidade(models.Model):
+    nome        = models.CharField(max_length=50)
+    estado      = models.ForeignKey(Estado, on_delete=models.PROTECT)
+    descricao   = models.TextField(
+        blank=True, null=True, verbose_name="Descrição",
+        help_text="Espaço para colocar qualquer informação."
+        )
+
+    def __str__(self):
+        return self.nome + ' - ' + self.estado.sigla
+
+class Pessoa(models.Model):
+
+    nome = models.CharField(max_length=50, verbose_name="Qual seu nome?", help_text="Digite seu nome completo")
+    nascimento = models.DateField(verbose_name='data de nascimento')
+    email = models.EmailField(max_length=100)
+
+    cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nome + ' - ' + str(self.nascimento)
