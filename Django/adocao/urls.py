dd0021915d5d89f881e90c535afa861c387fdce9
Aluno <rafael.zottesso@ifpr.edu.br>
2019-05-24 21:31:50 -0300

    Ajuste nos modelos e criação do estado baseado no CreateView.

--- Django/adocao/urls.py
+++ Django/adocao/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('sobre/', SobreView.as_view(), name="sobre"),
     path('contato/', ContatoView.as_view(), name="contato"),
     path('curriculo/', CurriculoView.as_view(), name="curriculo"),
+
+    # URLS de cadastros
+    path('cadastrar/estado/', EstadoCreate.as_view(), name="cadastrar-estado"),
+
 ]
--- Django/adocao/views.py
+++ Django/adocao/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+# Importa todas as classes do models.py
+from .models import *
+
+# Importa função que vai chamar as urls pelo "name" delas
+from django.urls import reverse_lazy
+
+# Importar as classes Views para inserir, alterar e excluir utilizando formulários
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+# Importa o TemplateView para criação de páginas simples
 from django.views.generic import TemplateView
 
 
@@ -10,16 +20,28 @@ from django.views.generic import TemplateView
 class PaginaInicialView(TemplateView):
     # Nome do arquivo que vai ser utilizado para renderizar
     # esta página/método/classe
-    template_name = "index.html"
+    template_name = "adocao/index.html"
 
 # Página "Sobre"
 class SobreView(TemplateView):
-    template_name = "sobre.html"
+    template_name = "adocao/sobre.html"
 
 # Página de contato
 class ContatoView(TemplateView):
-    template_name = "contato.html"
+    template_name = "adocao/contato.html"
 
 # Página de Currículo
 class CurriculoView(TemplateView):
-    template_name = "curriculo.html"
+    template_name = "adocao/curriculo.html"
+
+
+##################### INSERIR ######################
+class EstadoCreate(CreateView):
+    # Define qual o modelo pra essa classe vai criar o form
+    model = Estado
+    # Qual o html que será utilizado?
+    template_name = "adocao/formulario.html"
+    # Pra onde redirecionar o usuário depois de inserir um registro. Informe o nome da url
+    success_url = reverse_lazy("index")
+    # Quais campos devem aparecer no formulário?
+    fields = ['sigla', 'nome']
